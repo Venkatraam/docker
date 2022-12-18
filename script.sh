1bba02a0527795f80fc7f50e58e79cff54cdc769
Venkatraam <119103654+Venkatraam@users.noreply.github.com>
2022-12-18 12:06:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 cd /root
 rm -rf docker ;
-git clone https://github.com/mycloudazure/docker.git ;
+git clone https://github.com/Venkatraam/docker.git ;
 docker system prune -af ;
 cd /root/docker ;
-docker build -t myawspk/besant22:myapache .
-docker push myawspk/besant22:myapache
+docker build -t venkatraam/nivas:myapache .
+docker push venkatraam/nivas:myapache
 docker rm -f mycontianer
 docker system prune -af
-docker run -itd -p 80:80 --name mycontianer myawspk/besant22:myapache
+docker run -itd -p 80:80 --name mycontianer venkatraam/nivas:myapache
